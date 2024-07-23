bce0a794410a0a504656c6bf0c0df9e61dd47546
Jon Robison <narfman0@gmail.com>
2024-07-23 14:53:42 -0400

    use betterproto over protobuf

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
 
-from backend.proto import message_pb2
+from backend.proto.message import Message
 
 app = FastAPI()
 
@@ -12,9 +12,9 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_bytes()
-            msg = message_pb2.Message()
-            msg.ParseFromString(data)
-            msg.text = f"Message text was: {msg.text}"
-            await websocket.send_bytes(msg.SerializeToString())
+            msg = Message().parse(data)
+            await websocket.send_bytes(
+                bytes(Message(text=f"Message text was: {msg.text}"))
+            )
     except WebSocketDisconnect:
         print("Received WebSocketDisconnect")
--- frontend/nk/ws.py
+++ frontend/nk/ws.py
@@ -1,14 +1,12 @@
 import websocket
 
-from nk.proto import message_pb2
+from nk.proto.message import Message
 
 ws = websocket.WebSocket()
 ws.connect("ws://localhost:7666/ws")
 for i in range(10):
-    msg = message_pb2.Message()
-    msg.text = f"Hello, Server {i}"
-    result = ws.send_bytes(msg.SerializeToString())
-    msg = message_pb2.Message()
-    msg.ParseFromString(ws.recv())
+    msg = Message(text=f"Hello, Server {i}")
+    result = ws.send_bytes(bytes(msg))
+    msg = Message().parse(ws.recv())
     print(msg.text)
 ws.close()
