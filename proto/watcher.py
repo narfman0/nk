fbbbc6458c94e0f2613d40c26c5a8169ebf45678
Jon Robison <narfman0@gmail.com>
2024-07-23 20:57:32 -0400

    add a filesystem watcher, critter comforts

---
+++ proto/watcher.py
@@ -0,0 +1,66 @@
+import sys
+import time
+import logging
+from typing import Optional
+import subprocess
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, FileSystemEvent
+
+
+class EventHandler(FileSystemEventHandler):
+    def __init__(self, logger: Optional[logging.Logger] = None) -> None:
+        super().__init__()
+        self.logger = logger or logging.root
+
+    def on_moved(self, event: FileSystemEvent) -> None:
+        super().on_moved(event)
+
+        what = "directory" if event.is_directory else "file"
+        self.logger.info(
+            "Moved %s: from %s to %s", what, event.src_path, event.dest_path
+        )
+        self.handle_changes(event)
+
+    def on_created(self, event: FileSystemEvent) -> None:
+        super().on_created(event)
+
+        what = "directory" if event.is_directory else "file"
+        self.logger.info("Created %s: %s", what, event.src_path)
+        self.handle_changes(event)
+
+    def on_deleted(self, event: FileSystemEvent) -> None:
+        super().on_deleted(event)
+
+        what = "directory" if event.is_directory else "file"
+        self.logger.info("Deleted %s: %s", what, event.src_path)
+        self.handle_changes(event)
+
+    def on_modified(self, event: FileSystemEvent) -> None:
+        super().on_modified(event)
+
+        what = "directory" if event.is_directory else "file"
+        self.logger.info("Modified %s: %s", what, event.src_path)
+        self.handle_changes(event)
+
+    def handle_changes(self, event: FileSystemEvent) -> None:
+        subprocess.run(["make", "deploy"])
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    path = sys.argv[1] if len(sys.argv) > 1 else "./nk"
+    event_handler = EventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
