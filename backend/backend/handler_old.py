f901e05ba52425356a7eb2652e4c4233c92f17b3
Jon Robison <narfman0@gmail.com>
2024-07-24 20:07:02 -0400

    split implementations

---
+++ backend/backend/handler_old.py
@@ -0,0 +1,40 @@
+import logging
+
+from fastapi import WebSocket
+from starlette.websockets import WebSocketDisconnect
+
+from nk.proto import Message, PlayerJoined
+from backend.models import Player
+
+logger = logging.getLogger(__name__)
+players: list[Player] = []
+
+
+async def handle_connected(websocket: WebSocket, player: Player):
+    def broadcast(message: Message):
+        for remote_player in players:
+            if not player == remote_player:
+                remote_player.messages.put(message)
+
+    try:
+        while True:
+            while not player.messages.empty():
+                message = player.messages.get()
+                logger.debug(f"Sending message {message} to {player.uuid}")
+                await websocket.send_bytes(bytes(message))
+            data = await websocket.receive_bytes()
+            msg = Message().parse(data)
+            if msg.player_join_request._serialized_on_wire:
+                player.uuid = msg.player_join_request.uuid
+                players.append(player)
+                logger.info(f"Player join request success: {player.uuid}")
+                broadcast(Message(player_joined=PlayerJoined(uuid=player.uuid)))
+            elif msg.text_message._serialized_on_wire:
+                broadcast(msg)
+            elif msg.player_update._serialized_on_wire:
+                broadcast(msg)
+            logger.debug(f"Handled message: {msg} from {player.uuid}")
+    except WebSocketDisconnect:
+        print("Received WebSocketDisconnect")
+        # TODO emit player left
+        players.remove(player)
--- backend/backend/main.py
+++ backend/backend/main.py
@@ -2,47 +2,19 @@ import logging
 from queue import Queue
 
 from fastapi import FastAPI, WebSocket
-from starlette.websockets import WebSocketDisconnect
 
-from nk.proto import Message, PlayerJoined
 from backend.logging import initialize_logging
 from backend.models import Player
+from backend.handler_old import handle_connected
 
 initialize_logging()
 logger = logging.getLogger(__name__)
 app = FastAPI()
-players: list[Player] = []
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     player = Player(uuid=None, messages=Queue())
-
-    async def broadcast(message: Message):
-        for remote_player in players:
-            if not player == remote_player:
-                remote_player.messages.put(message)
-
-    try:
-        while True:
-            while not player.messages.empty():
-                message = player.messages.get()
-                logger.debug(f"Sending message {message} to {player.uuid}")
-                await websocket.send_bytes(bytes(message))
-            data = await websocket.receive_bytes()
-            msg = Message().parse(data)
-            if msg.player_join_request._serialized_on_wire:
-                player.uuid = msg.player_join_request.uuid
-                players.append(player)
-                logger.info(f"Player join request success: {player.uuid}")
-                await broadcast(Message(player_joined=PlayerJoined(uuid=player.uuid)))
-            elif msg.text_message._serialized_on_wire:
-                await broadcast(msg)
-            elif msg.player_update._serialized_on_wire:
-                await broadcast(msg)
-            logger.debug(f"Handled message: {msg} from {player.uuid}")
-    except WebSocketDisconnect:
-        print("Received WebSocketDisconnect")
-        # TODO emit player left
-        players.remove(player)
+    await handle_connected(websocket, player)
+    logger.info(f"Player disconnected: {player.uuid}")
