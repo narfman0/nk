333db6a5f9fbfe445b58d92120871ab61c3219f3
Jon Robison <narfman0@blastedstudios.com>
2024-08-01 16:16:30 -0400

    allow for direct mongo url setting

--- backend/nk/db/db.py
+++ backend/nk/db/db.py
@@ -5,12 +5,14 @@ from fastapi_users.db import BeanieBaseUser
 from fastapi_users_db_beanie import BeanieUserDatabase
 from motor.motor_asyncio import AsyncIOMotorClient
 
-DB_USER = environ.get("MONGODB_USER", "root")
-DB_PASS = environ.get("MONGODB_PASS", "rootpass")
-DB_HOST = environ.get("MONGODB_HOST", "localhost")
-DB_PORT = int(environ.get("MONGODB_PORT", "27017"))
+DATABASE_URL = environ.get("MONGODB_URL")
 DB_NAME = environ.get("MONGODB_NAME", "nk")
-DATABASE_URL = f"mongodb://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}"
+if not DATABASE_URL:
+    DB_USER = environ.get("MONGODB_USER", "root")
+    DB_PASS = environ.get("MONGODB_PASS", "rootpass")
+    DB_HOST = environ.get("MONGODB_HOST", "localhost")
+    DB_PORT = int(environ.get("MONGODB_PORT", "27017"))
+    DATABASE_URL = f"mongodb://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}"
 client = AsyncIOMotorClient(DATABASE_URL, uuidRepresentation="standard")
 db = client[DB_NAME]
 
