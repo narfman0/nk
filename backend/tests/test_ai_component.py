19a49e3dbff0000bac5e0d215bb058ac0046b6e7
Jon Robison <narfman0@blastedstudios.com>
2024-08-08 10:12:48 -0400

    elaborate ai component a tad

--- backend/tests/test_ai_component.py
+++ backend/tests/test_ai_component.py
@@ -1,11 +1,21 @@
 from unittest.mock import Mock
 
+from nk.models import Player
 from nk.ai_component import AiComponent
+from nk.world import World
+from nk_shared.models.zone import EnemyGroup
 
 
 class TestAiComponent:
     def test_trivial_update(self):
-        world_mock = Mock()
-        zone_mock = Mock(enemy_groups=[])
-        ai = AiComponent(world_mock, zone_mock)
+        world = World()
+        player = Player(user_id="testuuid", start_x=10, start_y=0)
+        world.players.append(player)
+        enemy_groups = [
+            EnemyGroup(
+                character_type_str="shadow_guardian", count=1, center_x=10, center_y=0
+            )
+        ]
+        zone = Mock(enemy_groups=enemy_groups)
+        ai = AiComponent(world, zone)
         ai.update(0.016)
