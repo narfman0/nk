5709b09bc964deabd7fd94749e0fe8f5fb6dbd56
Jon Robison <narfman0@blastedstudios.com>
2024-08-09 11:06:19 -0400

    use pytest fixtures

--- backend/tests/test_ai_component.py
+++ backend/tests/test_ai_component.py
@@ -1,21 +1,45 @@
 from unittest.mock import Mock
 
+import pytest
+
 from nk.models import Player
 from nk.ai_component import AiComponent
 from nk.world import World
 from nk_shared.models.zone import EnemyGroup
 
 
+@pytest.fixture
+def player() -> Player:
+    return Player(user_id="testuuid", start_x=10, start_y=0)
+
+
+@pytest.fixture
+def single_enemy_enemy_group() -> EnemyGroup:
+    return EnemyGroup(
+        character_type_str="shadow_guardian", count=1, center_x=10, center_y=0
+    )
+
+
+@pytest.fixture
+def world(player) -> World:
+    world = World()
+    world.players.append(player)
+    return world
+
+
+@pytest.fixture
+def zone(single_enemy_enemy_group) -> AiComponent:
+    return Mock(enemy_groups=[single_enemy_enemy_group], environment_features=[])
+
+
+@pytest.fixture
+def ai(world, zone) -> AiComponent:
+    return AiComponent(world, zone)
+
+
 class TestAiComponent:
-    def test_trivial_update(self):
-        world = World()
-        player = Player(user_id="testuuid", start_x=10, start_y=0)
-        world.players.append(player)
-        enemy_groups = [
-            EnemyGroup(
-                character_type_str="shadow_guardian", count=1, center_x=10, center_y=0
-            )
-        ]
-        zone = Mock(enemy_groups=enemy_groups)
-        ai = AiComponent(world, zone)
+    def test_trivial_update(self, ai: AiComponent):
         ai.update(0.016)
+
+    def test_closest_player(self, ai: AiComponent, player: Player):
+        assert ai.closest_player(0, 0) == player
