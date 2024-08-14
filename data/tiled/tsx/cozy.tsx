21b18e2d83df4318ee839a1dd8860a002fefdb8d
Jon Robison <narfman0@gmail.com>
2024-08-13 22:48:04 -0400

    add cozy tsx

---
+++ data/tiled/tsx/cozy.tsx
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="cozy" tilewidth="32" tileheight="32" tilecount="160" columns="16">
+ <image source="../images/cozy.png" width="512" height="320"/>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="18" y="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="18" y="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="17"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="17" y="15" width="1"/>
+   <object id="3" x="17.2727" y="16.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="18.9091" y="14.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.8182" y="14.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="64">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="18.3636" y="16.3636"/>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.1818" y="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="20.3636" y="16.7273" width="0.181818"/>
+   <object id="2" x="19.0909" y="16.5455"/>
+  </objectgroup>
+ </tile>
+ <tile id="67">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="18.7273" y="14.9091"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.1818" y="17.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="146">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14" y="14.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="147">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12.3636" y="12.7273"/>
+  </objectgroup>
+ </tile>
+</tileset>
