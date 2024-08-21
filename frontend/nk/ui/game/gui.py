1e2e5e5f3811cc3ca4e060fc3e4e4cf86c946d43
Jon Robison <narfman0@gmail.com>
2024-08-20 22:51:28 -0400

    addd ewma dt for performance testing

--- frontend/nk/ui/game/gui.py
+++ frontend/nk/ui/game/gui.py
@@ -9,25 +9,16 @@ class GameGui:
     def __init__(self):
         self.font = pygame.font.Font(f"{NK_DATA_ROOT}/fonts/HarmonicFont.ttf", 24)
         self.manager = pygame_gui.UIManager((WIDTH, HEIGHT))
-        width_height = (256, 50)
-        left = 32
-        self._hp_label = pygame_gui.elements.UILabel(
-            relative_rect=pygame.Rect((left, 32), width_height),
-            text="",
-            manager=self.manager,
-        )
-        self._debug_label = pygame_gui.elements.UILabel(
-            relative_rect=pygame.Rect((left, 64), width_height),
-            text="",
-            manager=self.manager,
-        )
+        self.dt_ewma = 0
 
     def draw(self, player: Character, surface: pygame.Surface):
         x, y = player.position
-        # self._hp_label.set_text(f"HP: {player.hp}")
-        # self._debug_label.set_text(f"x,y: {int(x)},{int(y)}")
         self.manager.draw_ui(surface)
 
+        text_sf = self.font.render(
+            f"dt ewma: {self.dt_ewma}", False, pygame.Color("white")
+        )
+        surface.blit(text_sf, (32, 0))
         text_sf = self.font.render(f"HP: {player.hp}", False, pygame.Color("white"))
         surface.blit(text_sf, (32, 32))
         text_sf = self.font.render(
@@ -37,3 +28,4 @@ class GameGui:
 
     def update(self, dt: float):
         self.manager.update(dt)
+        self.dt_ewma = 0.95 * self.dt_ewma + 0.05 * dt
